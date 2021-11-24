7e7bcfe82a033a23fe8d5e6031403e4f6597c1c4
Sam Minot <sminot@fredhutch.org>
2021-11-23 16:09:33 -0800

    Install garnett for monocle3

--- install.R
+++ install.R
@@ -1,18 +1,23 @@
-# First install Bioconductor and Monocle
+# First install Bioconductor and Monocle 3
 if (!requireNamespace("BiocManager"))
     install.packages("BiocManager", repos='http://cran.us.r-project.org')
 
 BiocManager::install()
 
-# Install monocle
-BiocManager::install(c("monocle"))
+# Next install a few more dependencies
+BiocManager::install(c('BiocGenerics', 'DelayedArray', 'DelayedMatrixStats',
+                       'limma', 'S4Vectors', 'SingleCellExperiment',
+                       'SummarizedExperiment'))
 
-# First install a few more dependencies
-BiocManager::install(c('DelayedArray', 'DelayedMatrixStats', 'org.Hs.eg.db', 'org.Mm.eg.db'))
-
-# Now install garnett
 install.packages("devtools", repos='http://cran.us.r-project.org')
-devtools::install_github("cole-trapnell-lab/garnett")
+devtools::install_github('cole-trapnell-lab/monocle3')
+
+
+# Install a few Garnett dependencies:
+BiocManager::install(c('org.Hs.eg.db', 'org.Mm.eg.db'))
+
+# Install Garnett
+devtools::install_github("cole-trapnell-lab/garnett", ref="monocle3")
 
 # Make sure that it was installed
 library(garnett)
